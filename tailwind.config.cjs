faace11c8ceaebd7ac4c87e61e09fa661c1334de
Erick Mireles <emireles.rosas@gmail.com>
2024-01-19 16:52:58 -0600

    Add new assets and update layout and navbar components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
-			title: ['Tanker', 'sans-serif']
+			title: ['Tanker', 'sans-serif'],
+			body: ['Poppins', 'sans-serif']
 		},
 		extend: {
 			letterSpacing: {
@@ -13,9 +16,15 @@ module.exports = {
 			fontSize: {
 				'10xl': ['10rem', '90%']
 			},
+			padding: {
+				page: '3rem',
+				'page-tablet': '2rem',
+				'page-mobile': '1rem'
+			},
 			colors: {
 				'dark-1': '#343B2B',
-				'dark-2': '#884415',
+				'dark-2': '#3F4D2D',
+				'dark-3': '#884415',
 				'brand-1': '#C6C66C',
 				'brand-2': '#DB8152',
 				'light-1': '#FBFBF9',
