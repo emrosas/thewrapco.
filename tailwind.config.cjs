a74133844c9ae405a072781671a7ac8143311d75
Erick Mireles <emireles.rosas@gmail.com>
2024-01-19 15:33:47 -0600

    Update dependencies and styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,24 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    fontFamily: {
-      title: ["Tanker", "sans-serif"],
-    },
-    colors: {
-      black: "#000",
-      white: "#FBFBF9",
-      orange: "#DB8152"
-    },
-    extend: {
-      letterSpacing: {
-        extrawide: '1rem',
-        superwide: '2.25rem',
-      },
-      fontSize: {
-        '10xl': ['10rem', '90%'],
-      }
-    },
-  },
-  plugins: [],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			title: ['Tanker', 'sans-serif']
+		},
+		extend: {
+			letterSpacing: {
+				extrawide: '1rem',
+				superwide: '2.25rem'
+			},
+			fontSize: {
+				'10xl': ['10rem', '90%']
+			},
+			colors: {
+				'dark-1': '#343B2B',
+				'dark-2': '#884415',
+				'brand-1': '#C6C66C',
+				'brand-2': '#DB8152',
+				'light-1': '#FBFBF9',
+				'light-2': '#FFD699'
+			}
+		}
+	},
+	plugins: []
+}
