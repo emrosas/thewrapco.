0c6e2af353762fe70ffe0777ce78ad0762e765cf
Erick Mireles <emireles.rosas@gmail.com>
2024-02-13 18:05:19 -0600

    Final tweaks to the navbar mobile menu

--- src/menuOpenAnimation.js
+++ src/menuOpenAnimation.js
@@ -1,23 +1,42 @@
 import gsap from 'gsap'
 
-const tl = gsap.timeline({ defaults: { duration: 1, ease: 'power1.out' }, paused: true }) // Create a timeline
-tl.to('#menu-button', { opacity: 0.2 })
-
+// Wait for the page to load before running the animation
 document.addEventListener('astro:page-load', () => {
+	// Create the timeline
+	const tl = gsap.timeline({ defaults: { duration: 0.5, ease: 'power1.out' }, paused: true }) // Create a timeline
+	tl.to('#nav-menu', {
+		display: 'flex',
+		opacity: 1,
+		position: 'fixed',
+		top: '0',
+		left: '0',
+		width: '100%',
+		height: '70%',
+		backgroundColor: '#343B2B',
+		borderRadius: '0 0 32px 32px'
+	})
+	tl.to('#menu-overlay', { display: 'block', opacity: 0.6 }, '=')
+
+	//Keep track of the menu state
 	let menuOpen = false
+
+	//Activate the animation on click of the menu button
 	const menuButton = document.getElementById('menu-button')
 	menuButton.addEventListener('click', () => {
 		if (menuOpen) {
-			tl.reverse()
+			tl.timeScale(2).reverse()
 			menuButton.setDirection(-1)
 			menuButton.play()
 			menuOpen = false
 		} else {
-			tl.play()
+			tl.timeScale(1).play()
 			menuButton.setDirection(1)
 			menuButton.play()
 			menuOpen = true
 		}
-		// menuButton.play()
+	})
+	const menuOverlay = document.getElementById('menu-overlay')
+	menuOverlay.addEventListener('click', () => {
+		menuButton.click()
 	})
 })
